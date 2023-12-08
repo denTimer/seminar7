e9683e73905b5ca9def27678fb8e0b99e69a59bc
«denTimer» <dentimer@yandex.ru>
2023-12-08 13:36:24 +0200

    Сделал 3 задание

--- zadanie1/Program.cs
+++ zadanie1/Program.cs
@@ -1,14 +1,13 @@
 ﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
 
 
-
         Console.WriteLine("Введите M:");
         int m = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine("Введите N:");
         int n = Convert.ToInt32(Console.ReadLine());
 
-        PrintNumbersInRange(m, n);
+            PrintNumbersInRange(m, n);
     
 
     void PrintNumbersInRange(int m, int n)
---
+++ zadanie3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+
+        int[] array = { 18, 23, 39, 42, 15, 26 };
+        System.Console.WriteLine("Исходный массив");
+        foreach (var element in array)
+        {
+        Console.Write(element + " ");
+        }
+        System.Console.WriteLine("");
+        System.Console.WriteLine("Развернутый массив");
+        PrintArrayReversed(array, array.Length - 1);
+    
+
+    void PrintArrayReversed(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(arr[index] + " ");
+            PrintArrayReversed(arr, index - 1);
+        }
+    }
+
